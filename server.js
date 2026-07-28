346d089add315d86b85ca695f55ecd83eca0113e
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-07-28 23:16:17 +0530

    creating and starting server in diff files

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
 /* 1... pack ko use krne keliye sbse pehle ek func. banaye  jisme "require("pack name ") likhnge aur console me func likhneg  fir terminal me node filename.js likhenge */
 
-const express = require('express'); //server created
+// const express = require('express'); //server created
 
-const app = express();//store krdiya ek app variable me .
+// const app = express();//store krdiya ek app variable me .
 
 
 /*yaha pe ab hm server ko program krenge  yaha pe hme app.get("/",(req,res) iska mtlb agar / api m koi request ayi to response jayega "hello world  👇👇*/
 
-app.get("/", (req, res) => {
-    res.send("hello YASIN YOUR SERVER IS RUNNING");
-})
+// app.get("/", (req, res) => {
+//     res.send("hello YASIN YOUR SERVER IS RUNNING");
+// })
 
 /* agar hm /about denge to jab ham browser me  'localhost:3000/about' likhnge to hme ye output milega  */
-app.get("/about", (req, res) => {
-    res.send("HELLO YASIN YOUR SERVER IS RUNNING YOURE IN ABOUT PAGE");
-})
+// app.get("/about", (req, res) => {
+//     res.send("HELLO YASIN YOUR SERVER IS RUNNING YOURE IN ABOUT PAGE");
+// })
 
-app.listen(3000) // server ko start kiya 3000 port no. hai .listen se start kiya server ko.
+// app.listen(3000) // server ko start kiya 3000 port no. hai .listen se start kiya server ko.
 
 //ab servers start krne k baad local browser me jake localhost:3000 likhnge result me eror get / aye mtlb serverstart hai 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.js    => creates the server.
+// server.js => starts the server.
+
+//ab server ko import krenge
+const app = require("./src/app");
+
+app.listen(3000,()=>{
+    console.log ("server is running on port no 3000")
+})//server ko start krenge
---
+++ src/app.js
@@ -0,0 +1,14 @@
+//sbse phle hm ek file banayenge src naam jisme ek file rhegi 'app.js' jaha ham server create krnge aur server.js file hai root me uska kaam rhega server ko start krna
+ 
+// app.js    => creates the server.
+// server.js => starts the server.
+
+const express = require ('express');
+
+
+
+
+const app = express();
+
+module.exports = app
+/** yaha se ab server ko export krenge */
