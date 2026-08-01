dabb354114db4b60f9112b7316b77c9e39401110
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-08-01 23:49:45 +0530

    added data to env file

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,12 @@
 /**mongoose ko require krenge database se connect krenge  */
 
 //db.js me hm sirf  logic likhte hain ki ki trha se server ko connect krnge database se 
+require("dotenv").config();
 
 const mongoose =require('mongoose');
 
 async function connectdb(){
- await mongoose.connect("mongodb+srv://aeccse2024mdyasinreza_db_user:JYNqqzAr48sd6hVT@comp-backend.2mmlrs9.mongodb.net/?appName=comp-backend");
+ await mongoose.connect(process.env.MONGO_URI);
 
 
 
