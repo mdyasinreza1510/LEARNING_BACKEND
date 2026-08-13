c9b6f1c8b95ce2ca7ee133f7b27f024fd8054383
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-08-13 23:11:36 +0530

    authentication processes

---
+++ AUTHENTICATION-BKND/src/routes/auth.routes.js
@@ -0,0 +1,15 @@
+const express = require ('express')
+
+
+
+//ab isi router se ham api create krnge 
+const router = express.Router()
+
+
+
+router.post('/register', async (req,res) =>{
+    
+})
+
+
+module.exports = router
