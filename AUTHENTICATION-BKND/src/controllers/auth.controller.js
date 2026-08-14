13eced734e3f3f481e19f7e1da3f35e6ae3dadf8
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-08-14 23:15:08 +0530

    added jwt package

--- AUTHENTICATION-BKND/src/controllers/auth.controller.js
+++ AUTHENTICATION-BKND/src/controllers/auth.controller.js
@@ -5,10 +5,16 @@
 
 3) FIR FUNCTUION KO OBJ K FORM ME EXPORT KRENGE Q KI HME US FUN. K MATERIALS KO USE KRNA HAI ISILIYE USE EK OBJ BANA K EXPORT KRENGE 
 
+4) fir ham ek user bvanayenge u1
+ ab mongo db me jo bhi data create hoga uski ek apni id hogi 10000 user = 10000 ids  aur sb different hogi 
+
+    - AB JAB SERVER USER KO DB ME REGISTER KREGA TO SERVER EK TOKEN GENERATE KREGA AUR TOKEN GENERATE HOGA JSONWEBTPKEN SE (JWT)
+    -
 */
 
 
-const usermodel = require('../models/user.model')
+const usermodel = require('../models/user.model');
+const jwt = require ('jsonwebtoken');
 
 
 
@@ -18,6 +24,13 @@ const usermodel = require('../models/user.model')
 async function registeruser(req,res){
 
     const {username, email, password } = req.body;
+
+
+
+    // yaha ham abhi ek user create krenge " u1 "
+    const user= usermodel.create({
+        username,email,password
+    })
 }
 
 
